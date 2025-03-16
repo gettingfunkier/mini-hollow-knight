180113c6ac810da1cbf6cf4ca77a3f37e69cc62f
gettingfunkier <dev.gettingfunkier@gmail.com>
2025-03-16 18:03:04 +0000

    Added charms & character monologues

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import random
 
 class Vessel:
-    def __init__(self, health = 30, soul = 0, power = 5, chance = 30):
+    def __init__(self, max_health, health, soul, power, chance):
+        self.max_health = max_health
         self.health = health
         self.soul = soul
         self.power = power
@@ -24,12 +25,12 @@ class Vessel:
     
     def soul_focus(self):
         if self.soul > 0:
-            if self.health < 30:
+            if self.health < self.max_health:
                 self.soul -= 1
                 self.health += 7 + random.randint(0, 3)
                 
-                if self.health > 30:
-                    self.health = 30 
+                if self.health > self.max_health:
+                    self.health = self.max_health 
                 print(f"\n- Vessel focuses 1 soul and heals to {self.health}hp! {self.soul} soul left")
                 return self.soul, self.health, False   
             else:
@@ -40,12 +41,7 @@ class Vessel:
             print(f"\nNot enough soul to heal!")
             return self.soul, self.health, True
         
-    def cast_spell(self, spell):
-        spells = {
-            "vengeful spirit": {"cost": 2, "damage": 15},
-            "howling wraiths": {"cost": 3, "damage": 25},
-        }
-
+    def cast_spell(self, spell, spells):
         if spell in spells:
             if self.soul >= spells[spell]["cost"]:
                 self.soul -= spells[spell]["cost"]
@@ -60,12 +56,13 @@ class Vessel:
     
 
 class Enemy:
-    def __init__(self, name, health, attack, chance, description):
+    def __init__(self, name, health, attack, chance, description, phrase):
         self.name = name
         self.health = health
         self.attack = attack
         self.chance = chance
         self.description = description
+        self.phrase = phrase
 
     def take_damage(self, damage):
         if random.randint(1, 100) <= self.chance:
@@ -76,32 +73,63 @@ class Enemy:
 
     def attack_player(self):
         damage = self.attack + random.randint(0, 2)
-        int = random.randint(1, len(self.description)) - 1
-        print(f"\n- {self.name} {self.description[int]}")
+        val = random.randint(1, len(self.description)) - 1
+        print(f"\n- {self.name} {self.description[val]}")
         return damage
     
+    def character_moto(self):
+        value = random.randint(0, len(self.phrase) - 1)
+        if self.name == "Lost Kin":
+            print(f'\n{self.phrase[value]}')
+        else:
+            print(f'\n"{self.phrase[value]}"')
+    
 
-def stats():
+def stats(knight):
     print(f"\nVessel: {knight.health}hp | {knight.soul} soul")
 
-knight = Vessel()
 enemies = [
-    Enemy("Lost Kin", health = 10, attack = 3, chance = 20, description = ["swings its nail around!", "spits a ball of radiance!"]),
-    Enemy("Soul Tyrant", health = 15, attack = 5, chance = 30, description = ["desolate dives onto the ground!", "casts a soul orb!"]),
-    Enemy("Nightmare King Grimm", health = 25, attack = 7, chance = 10, description = ["summons his dragons!", "dashes into the air!", "spawns fire from below!"])
+    {"name": "Lost Kin", "health": 10, "attack": 3, "chance": 15, "description": ["swings its nail around!", "spits a ball of radiance!"], "phrase": ["*screams*", "*yells*"]},
+    {"name": "Soul Tyrant", "health": 18, "attack": 5, "chance": 20, "description": ["desolate dives onto the ground!", "casts a soul orb!"], "phrase": ["The King falls, but I live forever!", "My dreams are eternal and so am I!"]},
+    {"name": "Nightmare King Grimm", "health": 26, "attack": 7, "chance": 35, "description": ["summons his dragons!", "dashes into the air!", "spawns fire from below!"], "phrase": ["This searing fire... It carries well the Ritual's promise.", "Dance with me, my friend. The crowd awaits. Show them you are worthy of a starring role!"]}
 ]
 
+charms = ["Mark of Pride", "Shaman Stone", "Lifeblood Heart"]
 
-def round():
-    print("\nWelcome to mini Hollow Knight!")
-    for enemy in enemies:
-        print(f"〉A NEW CHALLENGER HAS APPEARED: {enemy.name}!! 〈")
+def pick_charm():
+    charm = int(input("\n1 - Mark of Pride\n2 - Shaman Stone\n3 - Lifeblood Heart\n\nPick a charm: "))
+
+    if charm == 1:
+        print("\nYou've chosen Mark of Pride!")
+    elif charm == 2:
+        print("\nYou've chosen Shaman Stone!")
+    elif charm == 3:
+        print("\nYou've chosen Lifeblood Heart!")
+    else: 
+        print("Invalid input!")
+
+    return charm
+
+
+def round(knight, spells):
+    for item in enemies:
+        enemy = Enemy(
+            name = item["name"], 
+            health = item["health"], 
+            attack = item["attack"], 
+            chance = item["chance"], 
+            description = item["description"], 
+            phrase = item["phrase"]
+            )
+
+        print(f"\n〉A NEW CHALLENGER HAS APPEARED: {enemy.name}!! 〈")
+        enemy.character_moto()
 
         while knight.health > 0 and enemy.health > 0:
             def act():
                 invalid = False
                 print("\n - - - NEW TURN - - -")
-                stats()
+                stats(knight)
 
                 action = input("Choose action [ attack / heal / spell ] : ").strip().lower()
 
@@ -117,7 +145,7 @@ def round():
 
                 elif action == "spell":
                     spell = input("Choose a spell [ Vengeful Spirit / Howling Wraiths ] : ")
-                    damage = knight.cast_spell(spell)
+                    damage = knight.cast_spell(spell, spells)
                     enemy.take_damage(damage)
                 
                 else:
@@ -137,6 +165,50 @@ def round():
             print(f"\n{enemy.name} has been defeated!")
 
     if knight.health > 0:
-        print("\nYou killed all the enemies, and saved the kingdom! Well done, mighty Vessel.")
+        print("\nYou killed all the enemies, and saved Hallownest! Well done, mighty Vessel.")
 
-round()
+
+def start():
+    print("\nWelcome to mini Hollow Knight!")
+    charm = None
+
+    while True:
+        menu = input("\nA - START\nB - Pick a charm\n\nChoose: ").strip().lower()
+        if menu == "a" and charm == None:
+            knight = Vessel(max_health = 30, health = 30, soul = 0, power = 5, chance = 30)
+            spells = {
+                "vengeful spirit": {"cost": 2, "damage": 15},
+                "howling wraiths": {"cost": 3, "damage": 25},
+            }
+            round(knight, spells)
+            break
+
+        elif menu == "a" and charm != None:
+            round(knight, spells)
+            break
+
+        elif menu == "b":
+            charm = pick_charm()
+
+            if charm == 1:
+                knight = Vessel(max_health = 30, health = 30, soul = 0, power = 7, chance = 30)
+                spells = {
+                    "vengeful spirit": {"cost": 2, "damage": 15},
+                    "howling wraiths": {"cost": 3, "damage": 25},
+                }
+            elif charm == 2:
+                knight = Vessel(max_health = 30, health = 30, soul = 0, power = 5, chance = 30)
+                spells = {
+                    "vengeful spirit": {"cost": 2, "damage": 19},
+                    "howling wraiths": {"cost": 3, "damage": 29},
+                }
+            elif charm == 3:
+                knight = Vessel(max_health = 35, health = 35, soul = 0, power = 5, chance = 30)
+                spells = {
+                    "vengeful spirit": {"cost": 2, "damage": 15},
+                    "howling wraiths": {"cost": 3, "damage": 25},
+                }
+        else:
+            print("Invalid input!")
+
+start()
