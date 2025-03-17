d37cb621bb2e8df51636b07979ac2e1be0bdba09
gettingfunkier <dev.gettingfunkier@gmail.com>
2025-03-17 11:29:05 +0000

    added staggering & death messages

--- game.py
+++ game.py
@@ -27,7 +27,7 @@ class Vessel:
         if self.soul > 0:
             if self.health < self.max_health:
                 self.soul -= 1
-                self.health += 7 + random.randint(0, 3)
+                self.health += 10 + random.randint(0, 3)
                 
                 if self.health > self.max_health:
                     self.health = self.max_health 
@@ -45,7 +45,7 @@ class Vessel:
         if spell in spells:
             if self.soul >= spells[spell]["cost"]:
                 self.soul -= spells[spell]["cost"]
-                print(f"- Vessel casts {spell.title()}, dealing {spells[spell]['damage']} damage!")
+                print(f"\n- Vessel casts {spell.title()}, dealing {spells[spell]['damage']} damage!")
                 return spells[spell]["damage"]
             else:
                 print("Not enough soul!")
@@ -56,13 +56,15 @@ class Vessel:
     
 
 class Enemy:
-    def __init__(self, name, health, attack, chance, description, phrase):
+    def __init__(self, name, stagger, health, attack, chance, description, phrase, death):
         self.name = name
+        self.stagger = stagger
         self.health = health
         self.attack = attack
         self.chance = chance
         self.description = description
         self.phrase = phrase
+        self.death = death
 
     def take_damage(self, damage):
         if random.randint(1, 100) <= self.chance:
@@ -83,15 +85,24 @@ class Enemy:
             print(f'\n{self.phrase[value]}')
         else:
             print(f'\n"{self.phrase[value]}"')
-    
+
+    def stagger_stage(self):
+        if self.health <= self.stagger[0]:
+            print(f"\n〉{self.name} staggers! {self.stagger[1]} 〈")
+            self.attack += round(self.attack / 2)
+            return True
+        else: return False
+
+    def enemy_dies(self):
+        print(f"\n{self.death}")
 
 def stats(knight):
     print(f"\nVessel: {knight.health}hp | {knight.soul} soul")
 
 enemies = [
-    {"name": "Lost Kin", "health": 10, "attack": 3, "chance": 15, "description": ["swings its nail around!", "spits a ball of radiance!"], "phrase": ["*screams*", "*yells*"]},
-    {"name": "Soul Tyrant", "health": 18, "attack": 5, "chance": 20, "description": ["desolate dives onto the ground!", "casts a soul orb!"], "phrase": ["The King falls, but I live forever!", "My dreams are eternal and so am I!"]},
-    {"name": "Nightmare King Grimm", "health": 26, "attack": 7, "chance": 35, "description": ["summons his dragons!", "dashes into the air!", "spawns fire from below!"], "phrase": ["This searing fire... It carries well the Ritual's promise.", "Dance with me, my friend. The crowd awaits. Show them you are worthy of a starring role!"]}
+    {"name": "Lost Kin", "stagger": [10, "It shivers and the infection glows brighter"], "health": 20, "attack": 4, "chance": 15, "description": ["swings its nail around!", "spits a ball of radiance!", "dashes across the room!"], "phrase": ["*screams*", "*yells*"], "death": "The infection scatters. An empty, broken vessel falls to the ground."},
+    {"name": "Soul Tyrant", "stagger": [12, "He gasps, leaking soul everywhere"], "health": 32, "attack": 5, "chance": 20, "description": ["desolate dives onto the ground!", "casts a soul orb!"], "phrase": ["The King falls, but I live forever!", "My dreams are eternal and so am I!"], "death": "He explodes into fireworks of soul and thunder."},
+    {"name": "Nightmare King Grimm", "stagger": [20, "He turns into a swarm of dragons, returning more fiery"], "health": 52, "attack": 8, "chance": 35, "description": ["summons his dragons!", "dashes into the air!", "spawns fire from below!"], "phrase": ["This searing fire... It carries well the Ritual's promise.", "Dance with me, my friend. The crowd awaits. Show them you are worthy of a starring role!"], "death": "His nightmarish form vanishes to the dream realm."}
 ]
 
 charms = ["Mark of Pride", "Shaman Stone", "Lifeblood Heart"]
@@ -111,23 +122,34 @@ def pick_charm():
     return charm
 
 
-def round(knight, spells):
+def game_round(knight, spells):
     for item in enemies:
         enemy = Enemy(
             name = item["name"], 
+            stagger = item["stagger"],
             health = item["health"], 
             attack = item["attack"], 
             chance = item["chance"], 
             description = item["description"], 
-            phrase = item["phrase"]
+            phrase = item["phrase"],
+            death = item["death"]
             )
 
         print(f"\n〉A NEW CHALLENGER HAS APPEARED: {enemy.name}!! 〈")
         enemy.character_moto()
 
+        global stagger_check 
+        stagger_check = False
+
         while knight.health > 0 and enemy.health > 0:
             def act():
                 invalid = False
+
+                global stagger_check
+
+                if stagger_check == False:
+                    stagger_check = enemy.stagger_stage()
+
                 print("\n - - - NEW TURN - - -")
                 stats(knight)
 
@@ -162,7 +184,7 @@ def round(knight, spells):
             print(f"\nYou were killed by {enemy.name}! May your shade find peace")
             break
         else:
-            print(f"\n{enemy.name} has been defeated!")
+            enemy.enemy_dies()
 
     if knight.health > 0:
         print("\nYou killed all the enemies, and saved Hallownest! Well done, mighty Vessel.")
@@ -175,35 +197,35 @@ def start():
     while True:
         menu = input("\nA - START\nB - Pick a charm\n\nChoose: ").strip().lower()
         if menu == "a" and charm == None:
-            knight = Vessel(max_health = 30, health = 30, soul = 0, power = 5, chance = 30)
+            knight = Vessel(max_health = 50, health = 50, soul = 0, power = 5, chance = 30)
             spells = {
                 "vengeful spirit": {"cost": 2, "damage": 15},
                 "howling wraiths": {"cost": 3, "damage": 25},
             }
-            round(knight, spells)
+            game_round(knight, spells)
             break
 
         elif menu == "a" and charm != None:
-            round(knight, spells)
+            game_round(knight, spells)
             break
 
         elif menu == "b":
             charm = pick_charm()
 
             if charm == 1:
-                knight = Vessel(max_health = 30, health = 30, soul = 0, power = 7, chance = 30)
+                knight = Vessel(max_health = 50, health = 50, soul = 0, power = 7, chance = 30)
                 spells = {
                     "vengeful spirit": {"cost": 2, "damage": 15},
                     "howling wraiths": {"cost": 3, "damage": 25},
                 }
             elif charm == 2:
-                knight = Vessel(max_health = 30, health = 30, soul = 0, power = 5, chance = 30)
+                knight = Vessel(max_health = 50, health = 50, soul = 0, power = 5, chance = 30)
                 spells = {
                     "vengeful spirit": {"cost": 2, "damage": 19},
                     "howling wraiths": {"cost": 3, "damage": 29},
                 }
             elif charm == 3:
-                knight = Vessel(max_health = 35, health = 35, soul = 0, power = 5, chance = 30)
+                knight = Vessel(max_health = 65, health = 65, soul = 0, power = 5, chance = 30)
                 spells = {
                     "vengeful spirit": {"cost": 2, "damage": 15},
                     "howling wraiths": {"cost": 3, "damage": 25},
